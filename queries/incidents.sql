73bdc9ede1e3ae6ab13108259f35e684452f2f27
Raphael Pierzina <raphael@hackebrot.de>
2024-03-12 12:27:02 +0100

    Reformat SQL in incidents.sql

--- queries/incidents.sql
+++ queries/incidents.sql
@@ -1,31 +1,53 @@
-# Incidents Table
+-- Incidents View
 SELECT
-source,
-incident_id,
-MIN(IF(root.time_created < issue.time_created, root.time_created, issue.time_created)) as time_created,
-MAX(time_resolved) as time_resolved,
-ARRAY_AGG(root_cause IGNORE NULLS) changes,
-FROM
-(
-SELECT 
-source,
-CASE WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.number')
-     WHEN source LIKE "pagerduty%" THEN JSON_EXTRACT_SCALAR(metadata, '$.event.data.id')
-     END AS incident_id,
-CASE WHEN source LIKE "github%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.issue.created_at'))
-     WHEN source LIKE "pagerduty%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.event.occurred_at'))
-     END AS time_created,
-CASE WHEN source LIKE "github%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.issue.closed_at'))
-     WHEN source LIKE "pagerduty%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.event.occurred_at'))
-     END AS time_resolved,
-REGEXP_EXTRACT(metadata, r"root cause: ([[:alnum:]]*)") as root_cause,
-CASE WHEN source LIKE "github%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.issue.labels'), '"name":"Incident"')
-     WHEN source LIKE "pagerduty%" THEN TRUE # All Pager Duty events are incident-related
-     END AS bug,
-FROM four_keys.events_raw 
-WHERE event_type LIKE "issue%" OR event_type LIKE "incident%" OR (event_type = "note" and JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.noteable_type') = 'Issue')
-) issue
-LEFT JOIN (SELECT time_created, changes FROM four_keys.deployments d, d.changes) root on root.changes = root_cause
-GROUP BY 1,2
-HAVING max(bug) is True
-;
+  source,
+  incident_id,
+  MIN(IF(root.time_created < issue.time_created, root.time_created, issue.time_created)) AS time_created,
+  MAX(time_resolved) AS time_resolved,
+  ARRAY_AGG(root_cause IGNORE NULLS) AS changes,
+FROM (
+  SELECT
+    source,
+    CASE
+      WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.number')
+      WHEN source LIKE "pagerduty%" THEN JSON_EXTRACT_SCALAR(metadata, '$.event.data.id')
+    END
+      AS incident_id,
+    CASE
+      WHEN source LIKE "github%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.issue.created_at'))
+      WHEN source LIKE "pagerduty%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.event.occurred_at'))
+    END
+      AS time_created,
+    CASE
+      WHEN source LIKE "github%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.issue.closed_at'))
+      WHEN source LIKE "pagerduty%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.event.occurred_at'))
+    END
+      AS time_resolved,
+    REGEXP_EXTRACT(metadata, r"root cause: ([[:alnum:]]*)") AS root_cause,
+    CASE
+      WHEN source LIKE "github%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.issue.labels'), '"name":"Incident"')
+      WHEN source LIKE "pagerduty%" THEN TRUE # All Pager Duty events are incident-related
+    END
+      AS bug,
+  FROM
+    four_keys.events_raw
+  WHERE
+    event_type LIKE "issue%"
+    OR event_type LIKE "incident%"
+    OR (event_type = "note" AND JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.noteable_type') = 'Issue')
+) AS issue
+LEFT JOIN (
+  SELECT
+    time_created,
+    changes
+  FROM
+    four_keys.deployments d,
+    d.changes
+) AS root
+ON
+  root.changes = root_cause
+GROUP BY
+  1,
+  2
+HAVING
+  MAX(bug) IS TRUE ;
