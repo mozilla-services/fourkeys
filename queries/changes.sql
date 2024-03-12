8c6c01ed7f861faea5086cb4df8924b80425f729
Raphael Pierzina <raphael@hackebrot.de>
2024-03-12 11:09:30 +0100

    Reformat SQL in changes.sql

--- queries/changes.sql
+++ queries/changes.sql
@@ -1,10 +1,16 @@
-# Changes Table
-SELECT 
-source,
-event_type,
-JSON_EXTRACT_SCALAR(commit, '$.id') change_id,
-TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(commit, '$.timestamp')),second) as time_created,
-FROM four_keys.events_raw e,
-UNNEST(JSON_EXTRACT_ARRAY(e.metadata, '$.commits')) as commit
-WHERE event_type = "push"
-GROUP BY 1,2,3,4
+-- Changes View: The `change_id` field is a Git commit's ID
+SELECT
+  source,
+  event_type,
+  JSON_EXTRACT_SCALAR(commit, '$.id') AS change_id,
+  TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(commit, '$.timestamp')),second) AS time_created,
+FROM
+  four_keys.events_raw e,
+  UNNEST(JSON_EXTRACT_ARRAY(e.metadata, '$.commits')) AS commit
+WHERE
+  event_type = "push"
+GROUP BY
+  1,
+  2,
+  3,
+  4
