841a2db32e87aed9dc10d1d6675ea8519845befe
Raphael Pierzina <raphael@hackebrot.de>
2024-03-12 11:18:51 +0100

    Add service field for service repo to changes.sql

--- queries/changes.sql
+++ queries/changes.sql
@@ -2,10 +2,16 @@
 SELECT
   source,
   event_type,
+  CASE
+    WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.repository.full_name')
+  END
+    AS service,
   JSON_EXTRACT_SCALAR(commit, '$.id') AS change_id,
-  TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(commit, '$.timestamp')),second) AS time_created,
+  TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(commit, '$.timestamp')),second) AS time_created
 FROM
   four_keys.events_raw e,
+  -- Create a row for each element in the array of `commits` from the `metadata` field.
+  -- The other fields in the fields in the row are repeated for each element.
   UNNEST(JSON_EXTRACT_ARRAY(e.metadata, '$.commits')) AS commit
 WHERE
   event_type = "push"
@@ -13,4 +19,5 @@ GROUP BY
   1,
   2,
   3,
-  4
+  4,
+  5
