c3c7fecce5863a5101e4fb8b3c4508e3a29d04b7
Raphael Pierzina <raphael@hackebrot.de>
2024-03-12 12:02:41 +0100

    Reformat SQL in deployments.sql

--- queries/deployments.sql
+++ queries/deployments.sql
@@ -1,71 +1,93 @@
-# Deployments Table
-
-WITH deploys_cloudbuild_github AS (# Cloud Build, Github
-      SELECT 
+-- Deployments View: For GitHub `deploy_id` is the ID of the Deployment Status.
+WITH
+  deploys_cloudbuild_github AS (  -- Cloud Build, GitHub, ArgoCD
+    SELECT
       source,
-      id as deploy_id,
+      id AS deploy_id,
       time_created,
-      CASE WHEN source = "cloud_build" then JSON_EXTRACT_SCALAR(metadata, '$.substitutions.COMMIT_SHA')
-           WHEN source like "github%" then JSON_EXTRACT_SCALAR(metadata, '$.deployment.sha')
-           WHEN source = "argocd" then JSON_EXTRACT_SCALAR(metadata, '$.commit_sha') end as main_commit,
-      CASE WHEN source LIKE "github%" THEN ARRAY(
-                SELECT JSON_EXTRACT_SCALAR(string_element, '$')
-                FROM UNNEST(JSON_EXTRACT_ARRAY(metadata, '$.deployment.additional_sha')) AS string_element)
-           ELSE ARRAY<string>[] end as additional_commits
-      FROM four_keys.events_raw 
-      WHERE (
-      # Cloud Build Deployments
-         (source = "cloud_build" AND JSON_EXTRACT_SCALAR(metadata, '$.status') = "SUCCESS")
-      # GitHub Deployments
-      OR (source LIKE "github%" and event_type = "deployment_status" and JSON_EXTRACT_SCALAR(metadata, '$.deployment_status.state') = "success")
-      # ArgoCD Deployments
-      OR (source = "argocd" AND JSON_EXTRACT_SCALAR(metadata, '$.status') = "SUCCESS")
-      )
-    ),
-    deploys_circleci AS (# CircleCI pipelines
-      SELECT
+      CASE
+        WHEN source = "cloud_build" THEN JSON_EXTRACT_SCALAR(metadata, '$.substitutions.COMMIT_SHA')
+        WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.deployment.sha')
+        WHEN source = "argocd" THEN JSON_EXTRACT_SCALAR(metadata, '$.commit_sha')
+      END
+        AS main_commit,
+      CASE
+        WHEN source LIKE "github%" THEN ARRAY( SELECT JSON_EXTRACT_SCALAR(string_element, '$') FROM UNNEST(JSON_EXTRACT_ARRAY(metadata, '$.deployment.additional_sha')) AS string_element)
+      ELSE
+        ARRAY<string>[]
+      END
+        AS additional_commits
+    FROM
+      four_keys.events_raw
+    WHERE
+      (
+        -- Cloud Build Deployments
+        (source = "cloud_build" AND JSON_EXTRACT_SCALAR(metadata, '$.status') = "SUCCESS")
+        -- GitHub Deployments
+        OR (source LIKE "github%" AND event_type = "deployment_status" AND JSON_EXTRACT_SCALAR(metadata, '$.deployment_status.state') = "success")
+        -- ArgoCD Deployments
+        OR (source = "argocd" AND JSON_EXTRACT_SCALAR(metadata, '$.status') = "SUCCESS") )
+  ),
+  deploys_circleci AS (  -- CircleCI pipelines
+    SELECT
       source,
       id AS deploy_id,
       time_created,
       JSON_EXTRACT_SCALAR(metadata, '$.pipeline.vcs.revision') AS main_commit,
       ARRAY<string>[] AS additional_commits
-      FROM four_keys.events_raw
-      WHERE (source = "circleci" AND event_type = "workflow-completed" AND JSON_EXTRACT_SCALAR(metadata, '$.workflow.name') LIKE "%deploy%" AND JSON_EXTRACT_SCALAR(metadata, '$.workflow.status') = "success")
-    ),
-    deploys AS (
-      SELECT * FROM
+    FROM
+      four_keys.events_raw
+    WHERE
+      (source = "circleci"
+        AND event_type = "workflow-completed"
+        AND JSON_EXTRACT_SCALAR(metadata, '$.workflow.name') LIKE "%deploy%"
+        AND JSON_EXTRACT_SCALAR(metadata, '$.workflow.status') = "success")
+  ),
+  deploys AS (
+    SELECT
+      *
+    FROM
       deploys_cloudbuild_github
-      UNION ALL
-      SELECT * FROM deploys_circleci
-    ),
-    changes_raw AS (
-      SELECT
+    UNION ALL
+    SELECT
+      *
+    FROM
+      deploys_circleci
+  ),
+  changes_raw AS (
+    SELECT
       id,
-      metadata as change_metadata
-      FROM four_keys.events_raw
-    ),
-    deployment_changes as (
-      SELECT
+      metadata AS change_metadata
+    FROM
+      four_keys.events_raw
+  ),
+  deployment_changes AS (
+    SELECT
       source,
       deploy_id,
       deploys.time_created time_created,
       change_metadata,
-      four_keys.json2array(JSON_EXTRACT(change_metadata, '$.commits')) as array_commits,
+      four_keys.json2array(JSON_EXTRACT(change_metadata, '$.commits')) AS array_commits,
       main_commit
-      FROM deploys
-      JOIN
-        changes_raw on (
-          changes_raw.id = deploys.main_commit
-          or changes_raw.id in unnest(deploys.additional_commits)
-        )
-    )
-
-    SELECT 
-    source,
-    deploy_id,
-    time_created,
-    main_commit,   
-    ARRAY_AGG(DISTINCT JSON_EXTRACT_SCALAR(array_commits, '$.id')) changes,    
-    FROM deployment_changes
-    CROSS JOIN deployment_changes.array_commits
-    GROUP BY 1,2,3,4;
+    FROM
+      deploys
+    JOIN
+      changes_raw
+    ON
+      ( changes_raw.id = deploys.main_commit OR changes_raw.id IN UNNEST(deploys.additional_commits) )
+  )
+SELECT
+  source,
+  deploy_id,
+  time_created,
+  main_commit,
+  ARRAY_AGG(DISTINCT JSON_EXTRACT_SCALAR(array_commits, '$.id')) AS changes,
+FROM
+  deployment_changes
+CROSS JOIN
+  deployment_changes.array_commits
+GROUP BY
+  1,
+  2,
+  3,
+  4;
