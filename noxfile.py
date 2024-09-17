62b73d3a12dc20b37071b8d25cf24a67a139bae0
Graham Beckley <gbeckley@mozilla.com>
2024-09-17 10:45:52 -0400

    Add "test" tag to unit test sessions

--- noxfile.py
+++ noxfile.py
@@ -71,7 +71,7 @@ def _session_tests(session, folder):
     )
 
 
-@nox.session(python=["3.12"])
+@nox.session(python=["3.12"], tags=["test"])
 @nox.parametrize("folder", FOLDERS)
 def py(session, folder):
     """Runs py.test for a folder using the specified version of Python."""
