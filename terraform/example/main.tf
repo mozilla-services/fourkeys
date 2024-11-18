eb759f5a2510df946fc39f2f7a49c05a378c273c
grahamalama <gbeckley@mozilla.com>
2024-11-18 10:45:22 -0500

    Remove Terraform & associated resources / scripts, and update documentation accordingly (#165)
    * Delete Terraform and directories used by Terraform including queries, schemas, and scripts
    
    * Delete `deprecated` directory
    
    * Remove references to now-obsolete setup instructions in documentation
    
    * Remove data refresh script
    
    Relied on `setup` dir assets, and isn't something we'll be using since we're running this system in production

--- terraform/example/main.tf
+++
@@ -1,8 +0,0 @@
-module "fourkeys" {
-  source              = "../modules/fourkeys"
-  project_id          = var.project_id
-  enable_apis         = var.enable_apis
-  region              = var.region
-  bigquery_region     = var.bigquery_region
-  parsers             = var.parsers
-}
